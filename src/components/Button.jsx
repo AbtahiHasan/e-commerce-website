3a4273f9d2bfd7170af233ccbe7dfc3c24c8101c
AbtahiHasan <anikidpersonal@gmail.com>
2023-03-24 14:33:53 +0600

    contact us page added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = ({children, className}) => {
+const Button = ({children, className, type}) => {
     return (
-        <button className={`bg-indigo-700 p-3 rounded text-white ${className}`}>
+        <button type={type} className={`bg-indigo-700 p-3 rounded text-white ${className}`}>
             {children}
         </button>
     );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Button from '../components/Button';
 import Nav from '../components/Nav';
 
 const Contact = () => {
     return (
-        <section>
+        <section className=''>
             <Nav/>
+            <h2 className='mt-[80px] text-center py-5 text-2xl font-bold'>Contact Us</h2>
+            <div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1425.6244387055658!2d90.34398517275476!3d22.690050043930576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37553407fbece487%3A0x5d069b9599d4414a!2sBarishal!5e0!3m2!1sen!2sbd!4v1679645729303!5m2!1sen!2sbd" width="100%" height="300" style={{border:0 }}allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div className='max-w-[1200px] mx-auto'>
+                <form className='flex flex-col max-w-[500px] mx-auto gap-3 mt-3' action="https://formspree.io/f/mvonbvzk" method="POST">
+                    <input type="text"  className='border-2 outline-0 p-3 rounded' name='username' autoComplete='off' placeholder='Your Name' required/>
+                    <input type="email" className='border-2 outline-0 p-3 rounded' name="email" autoComplete='off' placeholder='Your Email'  required />
+                    <textarea  className='border-2 outline-0 p-3 rounded' name="massage" cols="30" rows="10" autoComplete='off' placeholder='massage' required ></textarea>
+                    <Button type="submit">Submit</Button>
+                </form>
+            </div>
         </section>
     );
 };
