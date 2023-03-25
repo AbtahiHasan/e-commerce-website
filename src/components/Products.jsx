1d0edafb093f1fd67e3f331c488bfff669a7320b
AbtahiHasan <anikidpersonal@gmail.com>
2023-03-25 13:27:51 +0600

    shop page done

---
+++ src/components/Products.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <section>
+  <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+    <ul class="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+      <li>
+        <a href="#" class="group block overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt=""
+            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+          />
+
+          <div class="relative bg-white pt-3">
+            <h3
+              class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+            >
+              Basic Tee
+            </h3>
+
+            <p class="mt-2">
+              <span class="sr-only"> Regular Price </span>
+
+              <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+            </p>
+          </div>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="group block overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt=""
+            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+          />
+
+          <div class="relative bg-white pt-3">
+            <h3
+              class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+            >
+              Basic Tee
+            </h3>
+
+            <p class="mt-2">
+              <span class="sr-only"> Regular Price </span>
+
+              <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+            </p>
+          </div>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="group block overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt=""
+            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+          />
+
+          <div class="relative bg-white pt-3">
+            <h3
+              class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+            >
+              Basic Tee
+            </h3>
+
+            <p class="mt-2">
+              <span class="sr-only"> Regular Price </span>
+
+              <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+            </p>
+          </div>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="group block overflow-hidden">
+          <img
+            src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            alt=""
+            class="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+          />
+
+          <div class="relative bg-white pt-3">
+            <h3
+              class="text-xs text-gray-700 group-hover:underline group-hover:underline-offset-4"
+            >
+              Basic Tee
+            </h3>
+
+            <p class="mt-2">
+              <span class="sr-only"> Regular Price </span>
+
+              <span class="tracking-wider text-gray-900"> £24.00 GBP </span>
+            </p>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
+
+    );
+};
+
+export default Products;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import Blog from '../components/Blog';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
+import Products from '../components/Products';
+import ProductSidebar from '../components/ProductSidebar';
+
 
 
 const Shop = () => {
     return (
         <div>
             <Nav/>
+            <ProductSidebar products={<Products/>}/>
+            
             <Footer/>
         </div>
     );
